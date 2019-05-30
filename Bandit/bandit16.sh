90c109d88b364803f6d58dff9109c2dcaf6a77c7
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-05-30 21:03:26 +1200

    finished 16

--- Bandit/bandit16.sh
+++ Bandit/bandit16.sh
@@ -5,12 +5,10 @@ expect "*: "
 send "cluFn7wTiGryunymYOu4RcffSxQluehd\r"
 expect "*\$ "
 
-send "nmap -p 31000-32000 localhost | grep /tcp | cut -c 1-5 | xargs -L1 openssl s_client -quiet -host localhost -port\r"
-expect "verify return:1\r"
-#send "cluFn7wTiGryunymYOu4RcffSxQluehd\r" 
+send "nmap -p 31000-32000 localhost | grep /tcp | cut -c 1-5\r"
 expect "*\$ "
 
-#send "echo cluFn7wTiGryunymYOu4RcffSxQluehd | openssl s_client -quiet -host localhost -port 31790"
-#expect "*\$ "
+send "echo cluFn7wTiGryunymYOu4RcffSxQluehd | openssl s_client -quiet -host localhost -port 31790\r"
+expect "*\$ "
 
 send "exit\r"
