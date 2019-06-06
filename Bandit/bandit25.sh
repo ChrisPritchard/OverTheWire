4c3a726901be6f44bd1ec05aaadd933e53442fa2
Christopher <chrispritchard.nz@gmail.com>
2019-06-06 18:19:14 +1200

    notes on use of resizing to escape more

--- Bandit/bandit25.sh
+++ Bandit/bandit25.sh
@@ -5,4 +5,14 @@ expect "*: "
 send "uNG9O58gUE7snukf3bvZ0rxhtnjzSGzG\r"
 expect "*\$ "
 
+send "cat /etc/passwd | grep bandit26\r"
+expect "*\$ "
+send "file /usr/bin/showtext\r"
+expect "*\$ "
+send "cat /usr/bin/showtext\r"
+expect "*\$ "
+
+# by resizing the terminal to a tiny size more will not finish
+# then we can run v to get into vi, then :e cat /etc/bandit_pass/bandit26 to get 5czgV9L3Xx8JPOyRbXh6lQbmIOWvPT6Z
+
 interact
