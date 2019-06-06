6830e7d4c94d8e819404c25cf2505b57d5da193f
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-06-06 20:26:40 +1200

    finished bandit 27

--- Bandit/bandit27.sh
+++ Bandit/bandit27.sh
@@ -4,3 +4,20 @@ spawn ssh -p 2220 bandit27@bandit.labs.overthewire.org
 expect "*: "
 send "3ba3118a22e93127a4ed485be72ef5ea\r"
 expect "*\$ "
+
+send "mkdir /tmp/git27\r"
+expect "*\$ "
+
+send "git clone ssh://bandit27-git@localhost/home/bandit27-git/repo /tmp/exploit27\r"
+expect "*(yes/no)? "
+send "yes\r"
+expect "*password: "
+send "3ba3118a22e93127a4ed485be72ef5ea\r"
+expect "*\$ "
+
+send "ls /tmp/exploit27\r"
+expect "*\$ "
+send "cat /tmp/exploit27/README\r"
+expect "*\$ "
+send "rm -rf /tmp/exploit27\r"
+expect "*\$ "
