fdc0ae086093a678ac969f56bdf6af2e3c0c2349
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-06-06 18:42:03 +1200

    notes for bandit25

--- Bandit/bandit25.sh
+++ Bandit/bandit25.sh
@@ -12,7 +12,8 @@ expect "*\$ "
 send "cat /usr/bin/showtext\r"
 expect "*\$ "
 
-# by resizing the terminal to a tiny size more will not finish
-# then we can run v to get into vi, then :e cat /etc/bandit_pass/bandit26 to get 5czgV9L3Xx8JPOyRbXh6lQbmIOWvPT6Z
+# by resizing the terminal to a tiny size, 'more' will not finish immediately
+# ssh in with command ssh -o "StrictHostKeyChecking no" -i bandit26.sshkey bandit26@localhost
+# type v to get into vi, then :e /etc/bandit_pass/bandit26 to get 5czgV9L3Xx8JPOyRbXh6lQbmIOWvPT6Z
 
 interact
