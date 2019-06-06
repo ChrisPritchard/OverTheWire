7002befd5702a8fa47cf5e4f5504224d8f723522
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-06-07 07:01:16 +1200

    solved 32 added 33 reader

--- Bandit/bandit32.sh
+++ Bandit/bandit32.sh
@@ -3,6 +3,13 @@
 spawn ssh -p 2220 bandit32@bandit.labs.overthewire.org
 expect "*: "
 send "56a9bf19c63d650ce78e6ec0354ee45e\r"
+expect ">> "
+
+send "\$0\r"
+expect "\$ "
+
+send "/bin/bash\r"
 expect "*\$ "
 
-interact
+send "cat /etc/bandit_pass/bandit33\r"
+expect "*\$ "
