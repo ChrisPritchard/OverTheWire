fe22d3471a1877ce0053086b9e3cf96a1238735f
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-06-08 20:02:21 +1200

    finished natas 11 and moved onto 12

--- Natas/natas11.fsx
+++ Natas/natas11.fsx
@@ -2,16 +2,23 @@ open System
 
 let encoded = "ClVLIh4ASCsCBE8lAxMacFMZV2hdVVotEhhUJQNVAmhSEV4sFxFeaAw="
 let json = "{\"showpassword\":\"no\",\"bgcolor\":\"#ffffff\"}"
+let targetJson = "{\"showpassword\":\"yes\",\"bgcolor\":\"#ffffff\"}"
 
-let base64_decode s = System.Convert.FromBase64String s |> Array.map char |> fun sa -> new String (sa)
-let base64_encode (s: string) = s |> Seq.map byte |> Seq.toArray |> System.Convert.ToBase64String
+let base64Decode s = System.Convert.FromBase64String s |> Array.map char |> fun sa -> String (sa)
+let base64Encode (s: string) = s.ToCharArray() |> Array.map byte |> System.Convert.ToBase64String
 
 let xor (inData: string) (key: string) =
     inData.ToCharArray ()
     |> Array.mapi (fun i c ->
-        char (byte c ^^^ byte key.[1]))
-    |> fun sa -> new String (sa)
+        char (byte c ^^^ byte key.[i % key.Length]))
+    |> fun sa -> String (sa)
 
-let secret = xor json (base64_decode encoded)
+let secret = xor json (base64Decode encoded)
 
-let test1 = base64_encode (xor json secret) = encoded
+let test1 = base64Encode (xor json secret)
+
+let secret2 = "qw8J"
+
+let test2 = base64Encode (xor json secret2)
+
+let solvedCookieValue = base64Encode (xor targetJson secret2)
---
+++ Natas/natas12.md
@@ -0,0 +1,5 @@
+# Natas 12
+
+1. Go to [http://natas12.natas.labs.overthewire.org](http://natas12.natas.labs.overthewire.org)
+2. Log in with natas12, EDXp0pS26wLKHZy1rDBPUZk0RKfLGIR3
+3. ?
