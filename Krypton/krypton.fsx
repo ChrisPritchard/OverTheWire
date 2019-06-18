2a5ef6c52f3ede109655ea5e00df4a27e5eb9d3c
Chris Pritchard <chrispritchard.nz@gmail.com>
2019-06-18 21:06:46 +1200

    further work on krypton4

--- Krypton/krypton.fsx
+++ Krypton/krypton.fsx
@@ -3,6 +3,7 @@ open System.Text
 open System.IO
 
 let dictionary = File.ReadAllLines "./Krypton/dictionary.txt"
+let englishFrequencyOrder = "eatsorinhcldupgfwymbkvjxqz".ToUpper().ToCharArray ()
 
 let b64decode s =
     let bytes = Convert.FromBase64String s
@@ -28,32 +29,39 @@ let rot n (s: string) =
                 char bound
     String(buffer)
 
-let freqDecode sources (data: string) =
-    let englishFrequencyOrder = "eatsorinhcldupgfwymbkvjxqz".ToUpper().ToCharArray ()
+let freqDecode sources (cipher: string) =
     let sourcesFrequencyOrder = 
         (sources |> Seq.map File.ReadAllText |> String.concat "").Replace(" ", "")
         |> Seq.countBy id |> Seq.sortByDescending snd |> Seq.map fst |> Seq.toArray
-    data.Replace(" ", "").ToCharArray ()
+    cipher.Replace(" ", "").ToCharArray ()
     |> Array.map (fun c -> 
         let index = Array.findIndex ((=) c) sourcesFrequencyOrder
         englishFrequencyOrder.[index])
-    |> fun (ca: char []) -> String(ca)
+    |> String
 
-let vigenèreDictionaryAttack keyLength (data: string) =
-    let concatted = data.Replace (" ", "")
-    let decode (key: string) =
-        concatted.ToCharArray ()
-        |> Array.mapi (fun i c -> 
-            let res = int c - int (key.[i % keyLength])
-            if res < minA then char (maxZ - (minA - (res + 1)))
-            else char res)
-        |> String
+let vigenèreDecode (key: string) (cipher: string) =
+    cipher.Replace(" ", "").ToCharArray() 
+    |> Array.mapi (fun i c ->
+        let keyChar = int key.[i % key.Length] - minA
+        rot -keyChar (string c) |> char)
+    |> String
+
+let vigenèreHack sources keyLength (cipher: string) =
+    let subStringsFor source =
+        (File.ReadAllText source).Replace (" ", "")
+        |> Seq.indexed
+        |> Seq.groupBy (fun (i, _) -> i % 6)
+        |> Seq.map (snd >> Seq.map snd >> Seq.toArray >> String)
+    let isLikely keyChar (subString: string) = 
+        rot -(int keyChar) subString 
+        |> Seq.countBy id
+        |> Map.ofSeq
+        |> fun m -> m.ContainsKey 'E' && float m.['E'] / float subString.Length > 0.12
+    let candidatesFor subString =
+        ['A'..'Z'] |> List.filter (fun c -> isLikely c subString)
+    ""
     
-    dictionary 
-    |> Array.filter (fun s -> s.Length = keyLength)
-    |> Array.map decode
-    |> Array.filter (fun decoded -> dictionary |> Array.exists decoded.Contains)
-
+    
 // Krypton 0:
 printfn "Krypton 1: %s" <| b64decode "S1JZUFRPTklTR1JFQVQ="
 
@@ -64,8 +72,9 @@ printfn "Krypton 2: %s" <| rot 13 "YRIRY GJB CNFFJBEQ EBGGRA"
 printfn "Krypton 3: %s" <| rot -12 "OMQEMDUEQMEK"
 
 // Krypton 3:
-let decode = freqDecode ["./Krypton/krypton03found/found1"; "./Krypton/krypton03found/found2"; "./Krypton/krypton03found/found3"]
-printfn "Krypton 4: %s" <| decode "KSVVW BGSJD SVSIS VXBMN YQUUK BNWCU ANMJS"
+let sources3 = ["./Krypton/krypton03found/found1"; "./Krypton/krypton03found/found2"; "./Krypton/krypton03found/found3"]
+printfn "Krypton 4: %s" <| freqDecode sources3 "KSVVW BGSJD SVSIS VXBMN YQUUK BNWCU ANMJS"
 
 // Krypton 4:
-printfn "Krypton 5 options:\r\n%A" <| vigenèreDictionaryAttack 6 "HCIKV RJOX"
+let sources4 = ["./Krypton/krypton04found/found1"; "./Krypton/krypton04found/found2"]
+printfn "Krypton 5 options:\r\n%A" <| vigenèreHack sources4 6 "HCIKV RJOX"
