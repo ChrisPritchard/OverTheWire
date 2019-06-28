3286820ff4358cddc01a40c0a5e7fcf4a42f0a04
Christopher <chrispritchard.nz@gmail.com>
2019-06-28 14:47:45 +1200

    renamed failed c file. work on shellcode

--- Narnia/narnia01-shellcode.c
+++ Narnia/narnia01-shellcode.c
@@ -1,12 +1,9 @@
-#include <unistd.h>
+#include <stdlib.h>
 
 int main()
 {
-    char command[] = {'/','b','i','n','/','c','a','t',0};
-    char target[] = {'.','/','n','a','r','n','i','a','0','0','.','m','d', 0};
-    char* argv[] = { command, target };
-    char* argp[] = {};
-    execve(command, argv, argp);
+    char command[] = {'/','b','i','n','/','s','h',0};
+    system(command);
 
     return 0;
 }
