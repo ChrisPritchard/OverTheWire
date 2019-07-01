859708dc2be59cddf90638728f3c64744f3a7feb
Christopher <chrispritchard.nz@gmail.com>
2019-07-01 17:26:03 +1200

    removed file that was messing me up

--- Narnia/narnia01/shellcode-testprog.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-const char shellcode[] = "\x48\x31\xff\x48\x83\xc7\x03\x48\x31\xc0\x48\x83\xc0\x3c\x0f\x05";
-
-int main()
-{
-    (*(void(*)())shellcode)();
-    // exit(0);
-}
